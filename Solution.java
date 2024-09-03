fce3e91f3b3bbccd0b5a7fdada222d5dc1fa3622
Abhilash <abhilashjoshi396@gmail.com>
2024-09-03 15:33:04 +0530

    initiL COMMIT

---
+++ Solution.java
@@ -0,0 +1,54 @@
+/***Write a Java program that converts a given integer into its written English form */
+import java.util.Scanner;
+
+public class Solution {
+    String[] belowTen = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    String[] belowTwenty = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    String[] belowHundred = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+
+    public String numberToWords(int num) {
+        if (num == 0) {
+            return "Zero";
+        }
+        if (num < 10) {
+            return belowTen[num];
+        }
+        if (num < 20) {
+            return belowTwenty[num - 10];
+        }
+        if (num < 100) {
+            return belowHundred[num / 10] + (num % 10 != 0 ? " " + belowTen[num % 10] : "");
+        }
+        if (num < 1000) {
+            return belowTen[num / 100] + " Hundred" + (num % 100 != 0 ? " " + numberToWords(num % 100) : "");
+        }
+        if (num < 1000000) {
+            return numberToWords(num / 1000) + " Thousand" + (num % 1000 != 0 ? " " + numberToWords(num % 1000) : "");
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Solution solution = new Solution();
+        
+        while (true) {
+            System.out.print("Enter a number (or type 'exit' to quit): ");
+            String input = scanner.nextLine();
+            
+            if (input.equalsIgnoreCase("exit")) {
+                break;
+            }
+            
+            try {
+                int num = Integer.parseInt(input);
+                String result = solution.numberToWords(num);
+                System.out.println("The number in words is: " + result);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid number.");
+            }
+        }
+
+        scanner.close();
+    }
+}
